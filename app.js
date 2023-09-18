ce65aebd21bae04a5179c360f9c368678c1110c8
daramm1 <145395775+daramm1@users.noreply.github.com>
2023-09-18 13:16:11 -0700

    Update app.js

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ document.getElementById('form').addEventListener('submit', function (event) {
     };
 
     // Send the form data via Email.js
-    emailjs.send("service_cczcb0i", "template_k7ixms9", formData)
+    emailjs.send("service_w56ho88", "template_le7pbdl", formData)
         .then(function (response) {
             console.log('SUCCESS!', response.status, response.text);
             redirectToURL();
